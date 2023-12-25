b0146a82f99e19467c5820b651c40e7fcc5575d7
rohan future devoloper <xrohaislam373@gmail.com>
2023-12-26 00:13:44 +0600

    first commit

---
+++ src/Component/HomePage/Hero/BigSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import useSomething from '@/Hooks/useSomething';
+const BigSection = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BigSection;
---
+++ src/Component/HomePage/Hooks/useSomething.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const useSomething = () => {
+    return Just_Another_Hand;
+};
+
+export default useSomething;
